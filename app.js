7b66004e5693f9506956b50c4cdfcfc3f2979ca0
Rishav <rishav.kundu98@gmail.com>
2018-02-11 11:44:33 +0530

    Commit 2

--- app.js
+++ app.js
@@ -5,19 +5,23 @@ app.get('/api/problemlist', (req, res) =>
     res.json([{
         'id': 1,
         'title': 'foo',
-        'body': 'okokok1'
+        'body': 'okokok1',
+        'testcase': 5
     }, {
         'id': 2,
         'title': 'bar',
-        'body': 'okokok2'
+        'body': 'okokok2',
+        'testcase': 5
     }, {
         'id': 3,
         'title': 'baz',
-        'body': 'okokok3'
+        'body': 'okokok3',
+        'testcase': 5
     }, {
         'id': 4,
         'title': 'fizz',
-        'body': 'okokok4'
+        'body': 'okokok4',
+        'testcase': 5
     }]));
 
 app.listen(5000, () => console.log('Example app listening on port 3001!'))
